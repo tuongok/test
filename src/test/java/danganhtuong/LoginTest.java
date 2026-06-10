199cda67b5f9ec5a126fefdfbcfbe18eb6115a2c
tuongok <danganhtuongg@gmail.com>
2026-06-10 13:08:39 +0700

    TUONGG

--- src/test/java/danganhtuong/LoginTest.java
+++ src/test/java/danganhtuong/LoginTest.java
@@ -43,8 +43,7 @@ public class LoginTest {
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.findElement(By.name("username")).sendKeys("Admin");
         
-        // Cố tình nhập sai mật khẩu nhưng lại mong đợi đăng nhập thành công
-        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.name("password")).sendKeys("admin1230");
         driver.findElement(By.cssSelector("button[type='submit']")).click();
         
         try { Thread.sleep(3000); } catch (Exception e) {}
